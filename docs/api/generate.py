8c533074865bb0df7d168ebb146550d7b95bca2c
Pekka Klärck <peke@iki.fi>
2024-10-04 16:05:57 +0300

    Print note about API doc generation

--- docs/api/generate.py
+++ docs/api/generate.py
@@ -57,7 +57,10 @@ def generate_api_docs(root: Path):
 # Add a navigation entry to the static `docs/api/index.md`.
 nav['API'] = 'index.md'
 
-if not os.getenv('DO_NOT_GENERATE_API_DOCS'):
+if os.getenv('DO_NOT_GENERATE_API_DOCS'):
+    print('Not generating API docs.')
+else:
+    print('Generating API docs.')
     generate_api_docs(root)
 
 # Generate navigation file for `literal-nav`.
