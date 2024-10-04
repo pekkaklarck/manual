53f8cf0ed0596a43875a62e9213d52d43a38ab48
Pekka Klärck <peke@iki.fi>
2024-10-04 16:04:58 +0300

    Add installation instructions.
    They are copied from INSTALL.md at the project root.

---
+++ docs/install/generate.py
@@ -0,0 +1,19 @@
+"""Generate installation instructions.
+
+They are copied from `INSTALL.md` at the project root.
+"""
+
+from pathlib import Path
+
+import mkdocs_gen_files
+
+
+root = Path(__file__).parent.parent.parent
+source = root / 'INSTALL.md'
+target = Path('install/index.md')
+
+print('Copying installation instructions.')
+with mkdocs_gen_files.open(target, 'w') as file:
+    file.write(source.read_text(encoding='UTF-8'))
+
+mkdocs_gen_files.set_edit_path(target, '..' / source.relative_to(root))
---
+++ docs/install/index.md
@@ -0,0 +1,3 @@
+# Robot Framework installation
+
+This page is replaced by INSTALL.md at the project root as part of the build process.
