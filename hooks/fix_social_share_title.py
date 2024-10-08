6e0b31b7560b82974a812b7145bb56828c388069
Pekka Klärck <peke@iki.fi>
2024-10-08 12:50:43 +0300

    Safer dictionary access

--- hooks/fix_social_share_title.py
+++ hooks/fix_social_share_title.py
@@ -1,10 +1,10 @@
-"""Hook to fix `og:title` on the front page.
+"""Hook to fix the social preview title (`og:title`) on the front page.
 
 https://github.com/squidfunk/mkdocs-material/discussions/7597
 """
 
 
 def on_page_context(context, page, config, nav):
-    for meta in page.meta['meta']:
+    for meta in page.meta.get('meta', []):
         if meta.get('property') == 'og:title' and meta.get('content') == 'Manual':
             meta['content'] = 'Robot Framework Manual'
