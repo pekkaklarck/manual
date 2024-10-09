ff6aec4b8fc848b68c4f6620308290d228059158
Pekka Klärck <peke@iki.fi>
2024-10-10 01:14:17 +0300

    Enhance setting social preview title on the home page.
    - Use `page.is_homepage` for checking is the current page the home
      page instead of checking the old content.
    - Change also `twitter:title`, not only `og:title`.

--- doc/manual/hooks/fix_social_share_title.py
+++ doc/manual/hooks/fix_social_share_title.py
@@ -1,10 +1,12 @@
-"""Hook to fix the social preview title (`og:title`) on the front page.
+"""Set the social preview title (`og:title`) on the front page to a custom value.
 
 https://github.com/squidfunk/mkdocs-material/discussions/7597
 """
 
 
 def on_page_context(context, page, config, nav):
-    for meta in page.meta.get('meta', []):
-        if meta.get('property') == 'og:title' and meta.get('content') == 'Manual':
-            meta['content'] = 'Robot Framework Manual'
+    if page.is_homepage:
+        for meta in page.meta['meta']:
+            if (meta.get('property') == 'og:title'
+                    or meta.get('name') == 'twitter:title'):
+                meta['content'] = 'Robot Framework Manual'
